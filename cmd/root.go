a4a7e8a47e103fc4db3091e43800a57e8b574ee2
dsolerh <dsolerh.cinter95@gmail.com>
2022-06-24 20:06:14 -0400

    update description and clean up

--- cmd/root.go
+++ cmd/root.go
@@ -10,18 +10,25 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "miggen",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A migration template based generator.",
+	Long: `A migration template based generator is uses a config file that is
+generated with the command "init". It creates a directory ".miggen" and inside
+a file ".config.{json|yaml}". The format for storage is json for default
+For example:
+	miggen init 			# prompt various config option
+	miggen init -y 		# create the config with default options
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+For generating the migration file use the command "create" with 
+the flags -type and -name. The type is optional and defaults to "blank"
+wich creates an empty file
+For example:
+	miggen create -name=<migration-name> -type=<custom-type>
+	miggen create -name=<migration-name> 		# with default type "blank"
+
+Custom types can be added to the config file .miggen/.config.{json|yaml}`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -45,7 +52,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
