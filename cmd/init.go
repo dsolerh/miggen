8f033147043a4e0a7ff1da89a59231690fcf7029
dsolerh <dsolerh.cinter95@gmail.com>
2022-06-24 20:06:36 -0400

    add functionalitie

--- cmd/init.go
+++ cmd/init.go
@@ -5,23 +5,86 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/dsolerh/miggen/config"
+	"github.com/dsolerh/miggen/pkg/dirutils"
 	"github.com/spf13/cobra"
 )
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Create the configuration needed for the cli.",
+	Long: `Create the configuration needed for the cli. It show a prompt
+for each configuration if the flag -y is not set.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+		cwd, err := os.Getwd()
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+			return
+		}
+		path := filepath.Join(cwd, config.DirName, config.ConfName)
+		confexist, err := dirutils.Exist(path)
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+			return
+		}
+		if confexist {
+			reader := bufio.NewReader(os.Stdin)
+			var override bool
+			fmt.Println("The config file already exist in this folder.")
+			for i := 0; i < 3; i++ {
+				fmt.Printf("You want to override it (Y/n)?")
+				data, err := reader.ReadString('\n')
+				if err != nil {
+					fmt.Printf("input error: %s\n", err.Error())
+					continue
+				}
+				data = strings.ToLower(data[:len(data)-1])
+				if data == "" || data == "y" || data == "yes" {
+					override = true
+					break
+				}
+				if data == "n" || data == "no" {
+					break
+				}
+			}
+			if !override {
+				return
+			}
+		}
+
+		defconfig, err := cmd.Flags().GetBool("yes")
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+			return
+		}
+
+		f, err := os.Create(path)
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+			return
+		}
+		defer f.Close()
+
+		encoder := json.NewEncoder(f)
+		encoder.SetIndent("", "\t")
+
+		conf := config.New()
+		if defconfig {
+			err = encoder.Encode(conf)
+			if err != nil {
+				fmt.Printf("err: %v\n", err)
+				return
+			}
+		}
+
 	},
 }
 
@@ -29,12 +92,5 @@ func init() {
 	rootCmd.AddCommand(initCmd)
 
 	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	initCmd.Flags().BoolP("yes", "y", false, "Use the default config")
 }
