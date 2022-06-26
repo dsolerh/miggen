82473b6a20b4e7e1b5aaa9d4071df1831e83c169
dsolerh <dsolerh.cinter95@gmail.com>
2022-06-26 07:34:45 -0400

    add command for generate migration

--- cmd/root.go
+++ cmd/root.go
@@ -5,9 +5,14 @@ Copyright © 2022 DANIEL SOLER <dsolerh.cinter95@gmail.com>
 package cmd
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/dsolerh/miggen/config"
+	"github.com/dsolerh/miggen/pkg/dirutils"
 	"github.com/spf13/cobra"
 )
 
@@ -18,9 +23,7 @@ var rootCmd = &cobra.Command{
 	Long: `A migration template based generator is uses a config file that is
 generated with the command "init". It creates a directory ".mig" and inside
 a file`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: MigCommand,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -35,4 +38,67 @@ func Execute() {
 func init() {
 	rootCmd.Flags().StringP("name", "n", "", "The migration name")
 	rootCmd.Flags().StringP("type", "t", config.DefaultType, "The migration type")
+}
+
+func MigCommand(cmd *cobra.Command, args []string) {
+	// get cwd
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+
+	path := filepath.Join(cwd, config.DirName, config.ConfName)
+	// check that the path exist
+	confexist, err := dirutils.Exist(path)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+
+	conf := config.New()
+
+	if confexist {
+		f, err := os.Open(path)
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+			return
+		}
+		defer f.Close()
+
+		decoder := json.NewDecoder(f)
+		err = decoder.Decode(conf)
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+			return
+		}
+	}
+
+	name, err := cmd.Flags().GetString("name")
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+	if name == "" {
+		fmt.Println("must specified a name for the migration")
+	}
+	typ, err := cmd.Flags().GetString("type")
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+
+	migName := time.Now().Format(conf.DateFormat) + conf.Separator + name + conf.Extension
+	f, err := dirutils.CreateAll(filepath.Join(cwd, conf.MigDir, migName))
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(conf.Types[typ])
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
 }
